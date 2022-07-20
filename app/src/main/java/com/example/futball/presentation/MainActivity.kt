97dc5afcfea97bd84f7e76d9207e4ca226edf659
kanake10 <10kanake@gmail.com>
2022-07-20 23:58:23 +0300

    commit

---
+++ app/src/main/java/com/example/futball/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.futball.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.futball.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
