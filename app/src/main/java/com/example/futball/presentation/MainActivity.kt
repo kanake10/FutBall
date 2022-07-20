b0d89988ae87eae7b55697e2ac84a5018b68449d
kanake10 <10kanake@gmail.com>
2022-07-21 02:44:47 +0300

    entry point di

--- app/src/main/java/com/example/futball/presentation/MainActivity.kt
+++ app/src/main/java/com/example/futball/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.futball.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.futball.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
