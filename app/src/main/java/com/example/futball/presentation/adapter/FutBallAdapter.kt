a4c17f882f1092a16bfd166902bc975af50684d7
kanake10 <10kanake@gmail.com>
2022-07-21 02:45:49 +0300

    nav

---
+++ app/src/main/java/com/example/futball/presentation/adapter/FutBallAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.futball.presentation.adapter
+
+import com.example.futball.domain.models.Players
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.futball.databinding.PlayerItemBinding
+
+
+class FutBallAdapter : RecyclerView.Adapter<FutBallAdapter.MyViewHolder>() {
+
+    private var listener :((Players)->Unit)?=null
+
+    var list = mutableListOf<Players>()
+
+    fun setContentList(list: MutableList<Players>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    class MyViewHolder(val viewHolder: PlayerItemBinding) :
+        RecyclerView.ViewHolder(viewHolder.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FutBallAdapter.MyViewHolder {
+        val binding =
+            PlayerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    fun itemClickListener(l:(Players)->Unit){
+        listener= l
+    }
+
+    override fun onBindViewHolder(holder: FutBallAdapter.MyViewHolder, position: Int) {
+        holder.viewHolder.players = this.list[position]
+        holder.viewHolder.root.setOnClickListener {
+            listener?.let {
+                it(this.list[position])
+            }
+        }
+
+    }
+    override fun getItemCount(): Int {
+        return this.list.size
+    }
+}
