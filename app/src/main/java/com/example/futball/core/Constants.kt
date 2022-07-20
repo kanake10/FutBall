b7beb6dd264581c1a64a98672442a3545b8c0ffe
kanake10 <10kanake@gmail.com>
2022-07-20 23:58:46 +0300

    commit

---
+++ app/src/main/java/com/example/futball/core/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.futball.core
+
+object Constants {
+    const val BASE_URL = "https://www.thesportsdb.com/api/v1/json/2/"
+    const val SEARCH_PLAYER= "searchplayers.php"
+    const val PLAYER_DETAILS= "lookupplayer.php"
+}
