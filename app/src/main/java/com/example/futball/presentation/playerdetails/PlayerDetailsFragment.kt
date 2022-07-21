a0480e48f2fc988ebf403ca2256c571e2ebf247a
kanake10 <10kanake@gmail.com>
2022-07-21 11:37:18 +0300

    commit

--- app/src/main/java/com/example/futball/presentation/playerdetails/PlayerDetailsFragment.kt
+++ app/src/main/java/com/example/futball/presentation/playerdetails/PlayerDetailsFragment.kt
@@ -1,60 +1,64 @@
 package com.example.futball.presentation.playerdetails
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.futball.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.coroutineScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.futball.databinding.FragmentPlayerDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PlayerDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class PlayerDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentPlayerDetailsBinding? = null
+    val binding: FragmentPlayerDetailsBinding
+        get() = _binding!!
+
+    private val viewModel: PlayerDetailsViewModel by viewModels()
+    private val args: PlayerDetailsFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player_details, container, false)
+        _binding = FragmentPlayerDetailsBinding.inflate(inflater, container, false)
+        return _binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlayerDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlayerDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        args.idPlayer?.let {
+            viewModel.getFutBallDetails(it)
+        }
+
+        lifecycle.coroutineScope.launchWhenCreated {
+            viewModel.mealDetails.collect {
+                if (it.isLoading) {
+                }
+                if (it.error.isNotBlank()) {
+                    Toast.makeText(requireContext(),it.error,Toast.LENGTH_SHORT).show()
+                }
+                it.data?.let {
+                    binding.playerdetails = it
                 }
             }
+        }
+
+        binding.detailsBackArrow.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
+
 }
